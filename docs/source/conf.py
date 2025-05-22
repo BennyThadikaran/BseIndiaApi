5f483d617c1a430c22d683dfec186588ad97255b
Benny Thadikaran <tbenny05@yahoo.com>
2025-05-22 14:40:22 +0530

    bump to version 2.0.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-src = Path(__file__).parents[2].resolve() / 'src'
+src = Path(__file__).parents[2].resolve() / "src"
 
 sys.path.insert(0, src.as_posix())
 
@@ -13,23 +13,23 @@ sys.path.insert(0, src.as_posix())
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'bse'
-copyright = '2023, Benny Thadikaran'
-author = 'Benny Thadikaran'
-release = '2.0.1'
+project = "bse"
+copyright = "2023, Benny Thadikaran"
+author = "Benny Thadikaran"
+release = "2.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
