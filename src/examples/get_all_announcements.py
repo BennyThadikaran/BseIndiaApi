9ed48ec379a9c010c27ae57d744bf4c55a076fb3
Benny Thadikaran <tbenny05@yahoo.com>
2026-04-25 11:07:02 +0530

    Minor improvements to examples/get_all_announcements.py
    - Output JSON file saved to same folder as script
    - Print file location of saved json file.
    - Other minor code improvements and formatting changes.

--- src/examples/get_all_announcements.py
+++ src/examples/get_all_announcements.py
@@ -1,6 +1,8 @@
 from bse import BSE
 import json
-'''
+import pathlib
+
+"""
 How to use BSE.announcements get all announcements.
 
 By default, only a single page of announcements is returned.
@@ -8,13 +10,15 @@ By default, only a single page of announcements is returned.
 We increment the 'page_no' argument to paginate and get all announcements.
 
 BSE can returns 2000+ announcements on a given day and result in 50+ requests.
-'''
+"""
 
+DIR = pathlib.Path(__file__).parent
+outfile = DIR / "announcements.json"
 ann: list[dict] = []
 total_count = 1000  # arbitary number to suppress pylint warnings
 page_count = 1
 
-with BSE('./') as bse:
+with BSE("./") as bse:
     while True:
         # Returns Nth page of announcements for the day upto current time.
         res = bse.announcements(page_no=page_count)
@@ -22,11 +26,11 @@ with BSE('./') as bse:
         if page_count == 1:
             # ROWCNT is the total number of announcements for the day
             # We only need this once on first request
-            total_count: int = res['Table1'][0]['ROWCNT']
+            total_count: int = res["Table1"][0]["ROWCNT"]
 
         page_count += 1
 
-        ann.extend(res['Table'])
+        ann.extend(res["Table"])
 
         if len(ann) >= total_count:
             break
@@ -35,8 +39,10 @@ with BSE('./') as bse:
         pct_completion = round(len(ann) / total_count * 100, 2)
 
         # Print on same line. \r is backspace to clear the previous print
-        print(f'{pct_completion}%', flush=True, end='\r' * 8)
+        print(f"{pct_completion}%", flush=True, end="\r")
 
 # Done save to file or anything else you need to do
-with open('announcements.json', 'w') as f:
+with open(outfile, "w") as f:
     json.dump(ann, f, indent=2)
+
+print(f"Announcements saved to {outfile}")
